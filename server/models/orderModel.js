e1abedbe78785ae653d1a03831dd709092b4c152
MUHAMMAD SHAHWAIZ MUGHAL <86921257+ShahwaizM@users.noreply.github.com>
2024-08-09 18:30:35 +0500

    Update orderModel.js

--- server/models/orderModel.js
+++ server/models/orderModel.js
@@ -6,7 +6,7 @@ const orderSchema = new mongoose.Schema(
     userId: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
-      required: true,
+      required: false,
     },
     name: { type: String, required: true },
     email: { type: String, required: true },
